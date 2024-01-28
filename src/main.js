feddf4f57572a199cd366344d342d5f0ac5ac729
Marco Ogaz-Vega <marcoogazvega@hotmail.com>
2024-01-28 14:36:07 -0800

    comments added to main

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
 //Marco Ogaz-Vega
 //1/24/24
 //CMPM 120
+// Rocket Patrol HD
+//approx 20 hours
+//UPDATE:
+// - Track a high score that persists across scenes and display it in the UI (1)
+// - Add your own (copyright-free) looping background music to the Play scene 
+// (keep the volume low and be sure that multiple instances of your music don't play when the game restarts) (1)
+// - Randomize each spaceship's movement direction at the start of each play (1)
+// - Create a new scrolling tile sprite for the background (1)
+// - Allow the player to control the Rocket after it's fired (1)
+// - Display the time remaining (in seconds) on the screen (3)
+// - random explosion sound for each hit (3)
+// - new rocket fire sound(1)
+// - Create a new enemy Spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (5)
+// - Implement parallax scrolling for the background (3)
+// 20 points total
+// all new sprites created by me, existing sprites from Rocket Patrol Tutorial created by Nathan Altice
 
 let config = {
     type: Phaser.AUTO,
@@ -20,16 +36,3 @@ let keyFIRE, keyRESET, keyLEFT, keyRIGHT
 
 let musicPlaying = false
 
-//UPDATE:
-// - Track a high score that persists across scenes and display it in the UI (1)
-// - Add your own (copyright-free) looping background music to the Play scene 
-// (keep the volume low and be sure that multiple instances of your music don't play when the game restarts) (1)
-// - Randomize each spaceship's movement direction at the start of each play (1)
-// - Create a new scrolling tile sprite for the background (1)
-// - Allow the player to control the Rocket after it's fired (1)
-// - Display the time remaining (in seconds) on the screen (3)
-// - random explosion sound for each hit (3)
-// - new rocket fire sound(1)
-// - Create a new enemy Spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (5)
-// - Implement parallax scrolling for the background (3)
-// 20 points total
